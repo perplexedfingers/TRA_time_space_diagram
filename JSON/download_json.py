ef015e1fbc66fc270361f3dd22a8f2fda6b1d996
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2021-07-27 22:17:30 +0800

    Rewrite 'download_json.py'

--- JSON/download_json.py
+++ JSON/download_json.py
@@ -1,76 +1,37 @@
-from urllib.request import Request, urlopen
-import urllib.request
-import zipfile
-import os
-import sys
+from pathlib import Path
+from urllib.parse import urlparse, urlunsplit
+from urllib.request import urlopen
+import csv
+import json
 
 from bs4 import BeautifulSoup
 
 
-def read_url(url):
-    url_json = []
+def downlaod_latest_json(root_csv_url: str) -> dict:
+    with urlopen(root_csv_url) as f:
+        lines = map(lambda s: s.decode(), f.readlines())
+    reader = csv.reader(lines)
 
-    url = url.replace(" ", "%20")
-    req = Request(url)
-    a = urlopen(req).read()
-    soup = BeautifulSoup(a, 'html.parser')
-    x = (soup.find_all('a'))
-    for i in x:
-        file_name = i.extract().get_text()
-        # print(file_name)
-        url_new = url + file_name
-        url_new = url_new.replace(" ", "%20")
-        if(file_name[-1] == '/' and file_name[0] != '.'):
-            read_url(url_new)
-        url_json.append([url_new, file_name])
+    json_file_list_url = next(r for r in reader if 'JSON' in r[0] or 'json' in r[0])[1]
+    with urlopen(json_file_list_url) as f:
+        html = BeautifulSoup(f, 'html.parser')
+    json_file_html_element = html.a  # First "a" element. Might be wrong
 
-    return url_json
-
-
-def download_tra_json(url_json):
-
-    ##    strUrl = 'http://163.29.3.98/json/'
-    ##    strFileExtent = '.zip'
-    ##    strdate = str_date
-    ##
-    # strFullUrl = strUrl + strdate + strFileExtent #完整網址
-    # strZipFile = str_date + strFileExtent #檔案名稱
-    ##
-    strLog = ''
-
-    try:
-        #strLog += '下載XML\n'
-        urllib.request.urlretrieve(url_json[0], url_json[1])
-
-        if os.path.exists(url_json[1]):
-            strLog += '下載成功' + '\n'
-        else:
-            strLog += '下載失敗' + '\n'
-
-       # strLog += '解壓縮XML\n'
-        zip_ref = zipfile.ZipFile(url_json[1], 'r')
-        zip_ref.extractall()
-        zip_ref.close()
-
-        if os.path.exists(url_json[1]):
-            strLog += '已刪除' + url_json[1] + '\n'
-            os.remove(url_json[1])
-        else:
-            strLog += '沒有' + url_json[1] + '\n'
-
-    except OSError as err:
-        strLog += "OS error: {0}".format(err) + '\n'
-    except ValueError:
-        strLog += "Could not convert data to an integer." + '\n'
-    except:
-        strLog += "Unexpected error:", sys.exc_info()[0] + '\n'
+    url_parse = urlparse(root_csv_url)
+    json_file_url = urlunsplit(
+        [url_parse.scheme, url_parse.netloc,
+         json_file_html_element['href'], '', '']
+    )
+    with urlopen(json_file_url) as f:
+        result = json.load(f)
+    return result
 
 
-items = read_url("http://163.29.3.98/json/")
-last_file_number = len(items) - 2
-download_tra_json(items[last_file_number])
-download_tra_json(items[1])
+if __name__ == '__main__':
+    # TODO maybe use args?
+    root_csv_url = 'https://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/8ae4cac374d0fbc50174d350a9aa04db'
+    file_name = 'timetable.json'
 
-# for item in read_url("http://163.29.3.98/json/"):
-# print(item[0])
-# download_tra_json(item)
+    timetable_json = downlaod_latest_json(root_csv_url)
+    dumped_json = json.dumps(timetable_json, indent=4, ensure_ascii=False)
+    Path(file_name).write_text(dumped_json, encoding='utf-8', errors='strict')
